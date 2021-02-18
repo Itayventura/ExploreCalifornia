91e68625938d9a9ec1191272c3b6701ca23e2944
itayventura <ventura.itay@gmail.com>
2021-02-18 23:27:33 +0200

    adding a api definition - description and cersion to the application

--- src/main/java/com/itayvenutra/exploreCalifornia/ExploreCaliforniaApplication.java
+++ src/main/java/com/itayvenutra/exploreCalifornia/ExploreCaliforniaApplication.java
@@ -1,21 +1,16 @@
 package com.itayvenutra.exploreCalifornia;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itayvenutra.exploreCalifornia.domain.Difficulty;
-import com.itayvenutra.exploreCalifornia.domain.Region;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
-
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
-
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Explore California API",
+				description = "API definitions of the Explore California Microservice",
+				version = "3.0.1"
+		))
 @SpringBootApplication
 public class ExploreCaliforniaApplication {
 
