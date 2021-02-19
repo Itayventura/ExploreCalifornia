3cd531a667a0db4ef6c793357ae17c9688db3452
itayventura <ventura.itay@gmail.com>
2021-02-19 12:00:47 +0200

    exposed some methods to everyone, including sign in method

---
+++ src/main/java/com/itayvenutra/exploreCalifornia/service/UserService.java
@@ -0,0 +1,32 @@
+package com.itayvenutra.exploreCalifornia.service;
+
+import com.itayvenutra.exploreCalifornia.repo.RoleRepository;
+import com.itayvenutra.exploreCalifornia.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager,
+                       RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Authentication signin(String username, String password) {
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+}
